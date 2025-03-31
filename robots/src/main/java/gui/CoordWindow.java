446873bac59676998b6f6cb912c310189d61f2a7
klinok066 <klinok066@gmail.com>
2025-03-31 20:08:38 +0500

    Task3. Реализовал новое окно для координат и сохранил его состояние

--- robots/src/main/java/gui/CoordWindow.java
+++ robots/src/main/java/gui/CoordWindow.java
@@ -3,26 +3,69 @@ package gui;
 import java.awt.BorderLayout;
 import java.awt.TextArea;
 import java.beans.PropertyVetoException;
+import java.util.Observable;
+import java.util.Observer;
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
+
+import model.ModelRobot;
 import serialization.Saveable;
 import serialization.State;
 
-public class CoordWindow extends JInternalFrame implements Saveable
+/**
+ * Класс CoordWindow представляет внутреннее окно, отображающее координаты робота.
+ * Реализует интерфейсы Saveable и Observer.
+ */
+public class CoordWindow extends JInternalFrame implements Saveable, Observer
 {
-    private TextArea textArea;
+    private final TextArea textArea;
+    private final ModelRobot modelRobot;
 
-    public CoordWindow()
+    /**
+     * Конструктор класса CoordWindow.
+     * @param modelRobot Модель робота
+     */
+    public CoordWindow(ModelRobot modelRobot)
     {
         super("Координаты робота", true, true, true, true);
         textArea = new TextArea();
-        textArea.setText("test1");
-        textArea.setText("test2");
-
+        textArea.setEditable(false);
+        textArea.setVisible(true);
+        this.modelRobot = modelRobot;
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(textArea, BorderLayout.CENTER);
         getContentPane().add(panel);
         pack();
+    }
+
+    /**
+     * Метод обновления окна при изменении состояния модели.
+     * @param o Наблюдаемый объект (модель робота)
+     * @param arg Аргумент (состояние модели)
+     */
+    @Override
+    public void update(Observable o, Object arg) {
+        if (o == null) {
+            return;
+        }
+
+        if (modelRobot.equals(o)) {
+            if (ModelRobot.key_robot_pos_changed.equals(arg)) {
+                onRobotPositionChanged();
+            }
+        }
+    }
+
+    /**
+     * Метод обработки изменения позиции робота.
+     * Обновляет текстовую область с новыми координатами робота.
+     */
+    private void onRobotPositionChanged() {
+
+        String coords = "x: " + ((int)modelRobot.getRobotX()) +
+                ", y: " + ((int)modelRobot.getRobotY() +
+                ", direction: " + ((int) (modelRobot.getDirection() * 180 / Math.PI)));
+        textArea.setText(coords);
     }
     @Override
     public State getState() {
