c160deae81c7927700d19aabf3f3a503cd682407
klinok066 <klinok066@gmail.com>
2025-03-31 18:52:27 +0500

    Task2. Реализация интерфейса Saveable

--- robots/src/main/java/gui/GameWindow.java
+++ robots/src/main/java/gui/GameWindow.java
@@ -1,14 +1,18 @@
 package gui;
 
-import java.awt.BorderLayout;
+import serialization.Saveable;
+import serialization.State;
+
+import java.awt.*;
+import java.beans.PropertyVetoException;
 
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
 
-public class GameWindow extends JInternalFrame
+public class GameWindow extends JInternalFrame implements Saveable
 {
     private final GameVisualizer m_visualizer;
-    public GameWindow() 
+    public GameWindow()
     {
         super("Игровое поле", true, true, true, true);
         m_visualizer = new GameVisualizer();
@@ -16,5 +20,43 @@ public class GameWindow extends JInternalFrame
         panel.add(m_visualizer, BorderLayout.CENTER);
         getContentPane().add(panel);
         pack();
+    }
+
+    @Override
+    public State getState() {
+        State state = new State();
+        state.setProperty("name", this.getName());
+        state.setProperty("height", this.getSize().height);
+        state.setProperty("width", this.getSize().width);
+        state.setProperty("location_x", this.getLocation().getX());
+        state.setProperty("location_y", this.getLocation().getY());
+        state.setProperty("is_hidden", this.isIcon);
+        return state;
+    }
+
+    @Override
+    public void loadState(State state) {
+        if (null == state) {
+            return;
+        }
+        Object height = state.getProperty("height");
+        Object width = state.getProperty("width");
+        long locationX = Math.round((Double) state.getProperty("location_x"));
+        long locationY = Math.round((Double) state.getProperty("location_y"));
+        Object isHidden = state.getProperty("is_hidden");
+
+        this.setSize(Math.toIntExact((Long) width), Math.toIntExact((Long) height));
+        this.setLocation(Math.toIntExact(locationX), Math.toIntExact(locationY));
+
+        try {
+            this.setIcon((Boolean) isHidden);
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "GameWindow";
     }
 }
